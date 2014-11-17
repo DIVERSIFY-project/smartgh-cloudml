98bcccb6c0152f6189b50b97a0dec1cb7b2a7bb0
songhui <fafeysong@gmail.com>
2014-11-17 23:51:57 +0100

    docker files

--- dockerfile/build.sh
+++ dockerfile/build.sh
@@ -1,2 +1,8 @@
 cd base
 docker build -t songhui/smhp-base .
+
+cd ../sensor
+docker build -t songhui/smhp-sensor .
+
+cd ../hopper
+docker build -t songhui/smhp-hopper .
---
+++ scripts/config-gh-launcher2.sh
@@ -0,0 +1,1 @@
+docker run -e "redisurl=192.168.2.11:9527" -i -t songhui/smhp-sensor /opt/gh/run_sensor.sh
